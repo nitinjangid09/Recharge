bc1c0975bd7b90471665895ef5982c0807ebc25c
Nitin Jangid <jangirnitin1999@gmail.com>
2026-03-05 18:03:48 +0530

    safeareaprovider

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
-import { toastConfig } from './src/utils/toastConfig';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Toast from "react-native-toast-message";
+
+import AppNavigator from "./src/navigation/AppNavigator";
+import { toastConfig } from "./src/utils/toastConfig";
 
 const App = () => {
-  return <AppNavigator />;
-  
-  <Toast config={toastConfig} />
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppNavigator />
+        <Toast config={toastConfig} />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
